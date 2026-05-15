c5789ccafd44f53c7ccc60145e07547413050d88
Devantics01 <devantics01@gmail.com>
2026-05-15 15:58:37 -0700

    login

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/accounts', accountRoutes);
 
 // Sync all database models in correct dependency order
-await sequelize.sync({ alter: true });
+await sequelize.sync();
 
 // Start background jobs
 startCronJobs();
